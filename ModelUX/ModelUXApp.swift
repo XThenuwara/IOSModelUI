a02a9934edbf2ee67c3596f035f0a7f8578c7eff
xthenuwara <xthenuwara@gmail.com>
2024-12-20 00:36:07 +0530

    Initial Commit

---
+++ ModelUX/ModelUXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelUXApp.swift
+//  ModelUX
+//
+//  Created by Yasas Hansaka Thenuwara on 2024-12-20.
+//
+
+import SwiftUI
+
+@main
+struct ModelUXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModelUXTests/ModelUXTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModelUXTests.swift
+//  ModelUXTests
+//
+//  Created by Yasas Hansaka Thenuwara on 2024-12-20.
+//
+
+import Testing
+@testable import ModelUX
+
+struct ModelUXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
