5864b9921bf9ee9dcde64e1e545b79c27db9d7c3
xthenuwara <xthenuwara@gmail.com>
2024-12-22 12:37:00 +0530

    initial commit

---
+++ ModelUX/lib/observables.swift
@@ -0,0 +1,12 @@
+//
+//  Observeable.swift
+//  ModelUX
+//
+//  Created by Yasas Hansaka Thenuwara on 2024-12-20.
+//
+import SwiftUI
+
+final class NavbarLeadDrawerObservable: ObservableObject {
+        
+    @Published var isLeadDrawerOpen: Bool = false
+}
